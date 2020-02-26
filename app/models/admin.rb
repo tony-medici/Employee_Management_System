7e9d6b8deaa8493228fa77d55abad9ffc803fadd
tony-medici <tonyudeagbala@gmail.com>
2020-02-26 12:48:17 +0000

    create migration file and password column, add has_secure_password as password validation to admin.rb,

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,6 @@ class Admin < ApplicationRecord
     validates :username, presence: true,
             uniqueness: {case_sensitive: false },
             length: { minimum: 3, maximum: 15 }
-            
+    
+    has_secure_password         
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_102804) do
+ActiveRecord::Schema.define(version: 2020_02_26_123757) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "employees", force: :cascade do |t|
