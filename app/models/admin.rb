212e9ecc4af563bd67980400c9670266f99cf3fc
tony-medici <tonyudeagbala@gmail.com>
2020-02-26 11:08:31 +0000

    Add validations to admin.rb file

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ApplicationRecord
-
-
+    validates :username, presence: true,
+            uniqueness: {case_sensitive: false },
+            length: { minimum: 3, maximum: 15 }
+            
 end
