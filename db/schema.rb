998111e9ae698f6bea08a2f2de394a3e8822b6a1
tony-medici <tonyudeagbala@gmail.com>
2020-02-26 10:43:22 +0000

    Create migration file for admin table, create admin username through the rails console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_193112) do
+ActiveRecord::Schema.define(version: 2020_02_26_102804) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "fullname"
